d118a0234d11182c02b6a5cb3ebfd260187a683a
email <tarunmehto71@gmail.com>
2022-11-11 23:40:09 +0530

    day4_done

---
+++ day-3-final_project/final.js
@@ -0,0 +1,20 @@
+function one(){
+    document.querySelector(".slide_image").src="https://s.rmjo.in/AC-Offer-Banner-MWeb-.jpg"
+}
+function two(){
+    document.querySelector(".slide_image").src="https://s.rmjo.in/Paytm-CB-offer-MWeb.jpg"
+}
+function three(){
+    document.querySelector(".slide_image").src="https://s.rmjo.in/Paytm-Payments-Bank-MWeb-banner.png"
+}
+function four(){
+    document.querySelector(".slide_image").src="https://s.rmjo.in/AirOKM-Web.png"
+}
+function five(){
+    document.querySelector(".slide_image").src="https://s.rmjo.in/WP-M-Web.png"
+}
+setInterval(one,2000)
+setInterval(two,4000)
+setInterval(three,6000)
+setInterval(four,8000)
+setInterval(five,10000)
